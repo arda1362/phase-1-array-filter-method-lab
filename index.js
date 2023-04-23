6452d18c27396e5cfd7850536f15586e947b2f67
Ardavan Zareirad <ardavan2525@gmail.com>
2023-04-23 19:53:17 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+const findMatching = (names, string) => {
+    return names.filter(names => names.toUpperCase() === string.toUpperCase())
+} 
+function fuzzyMatch(names,string){
+    const driversMatch = [];
+    for(const drivers of names){
+        if( string === names)
+        {
+            return driversMatch.push(drivers)
+        }
+    }
+    return  names.filter(names => names[0] === string[0])
+}
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
+
+
+
+
+
